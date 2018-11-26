48078299417c73ecc954df6061c2370e40784829
VolVoX94 <S58kih94>
2018-11-26 10:41:41 +0100

    Rating functionality added

--- app/src/main/java/com/example/alexa/carstop/activity/FinishActivity.java
+++ app/src/main/java/com/example/alexa/carstop/activity/FinishActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-
 import com.example.alexa.carstop.R;
 
 public class FinishActivity extends AppCompatActivity {
